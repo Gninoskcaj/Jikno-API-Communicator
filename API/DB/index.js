35874595f02dfb7ddc9112f6da6d52d951e53273
Jackson <jackson.mooring@gmail.com>
2019-06-14 17:06:43 -0500

    Keep chuging

--- API/DB/index.js
+++ API/DB/index.js
@@ -1,44 +1,47 @@
 // Params: username, table, method
-const users = [
-	{
-		username: 'Jeff',
-		password: '12',
-		id: '1333'
-	},
-	{
-		username: 'Bill',
-		password: '1',
-		id: '1234'
-	}
-]
-
-
-
-
+const users = () => {
+	return [
+		{
+			username: 'Jeff',
+			password: '12',
+			id: '1333'
+		},
+		{
+			username: 'Bill',
+			password: '1',
+			id: '1234'
+		}
+	]
+}
 
 const start = (username, table, method, password) => {
-	let url = `https://api.jikno.com/${table.toLowerCase()}/${userId()}`
+	//let url = `https://api.jikno.com/${table.toLowerCase()}/${userId()}`
 	let userId = '';
+	return userId(username, password, users())
+	
 
 }
 
-const userId = (username, password) => {
+const userId = (username, password, users) => {
 	for (let x in users) {
 		for (let i in users[x]) {
 			if (users[x].username === username && users[x].password === password) {
-				return users[x].id
+				return handleUserId(users[x].username, users[x].passwordusers[x].id)
 			}
 		}
 	}
 }
 
-const handleUserId = (username, password) => {
+const handleUserId = (username, password, id) => {
+	console.log(username)
+	console.log(userId(username, password))
+	console.log(password)
 	if (!userId(username, password)) {
 		return {
 			status: 404,
-			message: `User with username: ${username}, does not exist`
-		} 
-	} else {
+			message: `Username or password does not match`
+		}
+	} else {	
 		return {
 			id: userId(username, password),
 			username: username,
@@ -50,4 +53,4 @@ const handleUserId = (username, password) => {
 }
 
 
-console.log(handleUserId('Jeff', '12'))
+console.log(start('Jeff', '12'))
