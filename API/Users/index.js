784713e9495c8f44328a7d23bdcd53f5baced564
Jackson <jackson.mooring@gmail.com>
2019-06-14 08:42:41 -0500

    create getUser method

--- API/Users/index.js
+++ API/Users/index.js
@@ -1,6 +1,31 @@
-module.exports.getUser = (url) => {
+import db from '../DB';
+
+const getUser = (table, method, username, password) => {
+	const res = {};
+
 	// Fetch User
+	const user = db.start(username, table, method);
+
+	// Get User Id
+	const userId = user.id;
+
 	// Confirm User
+	const userPassword = user.data.password === password;
+	const userId = user.data.username === username;
+
 	// Validate Fetch
+	const status = user.res.status;
+
+	if (status > 300) {
+		res.status = 500;
+		res.message = user.res.message;
+	}
+
 	// Send Response
+
+	return {
+		status: res.status,
+		user: user.data,
+		message: res.message,
+	}
 }
